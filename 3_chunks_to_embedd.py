293c9e711d3c15216749c312689a16ac0c676c6b
Kumar_Ansuman <ansusibu717@gmail.com>
2026-06-29 11:39:17 +0530

    feat: implement vector embedding storage

---
+++ 3_chunks_to_embedd.py
@@ -0,0 +1,58 @@
+import os
+import json
+from langchain_core.documents import Document
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+PERSIST_DIRECTORY = "./chroma_gemini_db"
+
+
+def load_json_to_langchain_docs(folder_path):
+
+    docs = []
+
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith('.json'):
+            file_path = os.path.join(folder_path, file_name)
+
+            with open(file_path, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                segments = data.get("segments", [])
+
+                for idx, segment in enumerate(segments):
+                    text_content = segment.get("text", "")
+                    if not text_content.strip():
+                        continue
+                    
+                    metadata = {
+                        "tutorial_number": int(segment.get("tutorial_number", 0)),
+                        "video_title": segment.get("video_title", ""),
+                        "start": float(segment.get("start", 0.0)),
+                        "end": float(segment.get("end", 0.0)),
+                        "source": file_name
+                    }
+
+                    doc = Document(page_content=text_content, metadata=metadata)
+                    docs.append(doc)
+
+    return docs
+
+
+folder_path = "./jsons"
+documents = load_json_to_langchain_docs(folder_path)
+
+print(f"Loaded {len(documents)} segments. Vectorizing ...")
+
+vector_store = Chroma.from_documents(
+    documents=documents,
+    embedding=embeddings,
+    persist_directory=PERSIST_DIRECTORY
+)
+
+print("Chroma Store built successfully using Gemini Embeddings!")
